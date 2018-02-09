e4fc106ba930c18b6c012c4920d01260871a9968
Juan Felipe <k9inhalo@hotmail.com>
2018-02-09 11:47:02 -0500

    commit 3

---
+++ MICAlculadora/src/micalculadora/Principal.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package micalculadora;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Principal {
+    public static void main(String[]args){ 
+        //crear objeto de la clase calculadora
+        Calculadora micalculadora; //definicion
+        micalculadora=new Calculadora(); //contruir el objeto
+                
+        
+    }
+    
+}
