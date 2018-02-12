28f98bef89bd809ca6f0a981f5e78dec33629e06
Estudiantes <Estudiantes@Labing70207>
2018-02-12 17:57:57 -0500

    comit 45

---
+++ JavaApplication8/src/javaapplication8/Calculadora.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication8;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Calculadora {
+
+    protected double x;
+    private double y;
+    private double r;
+
+    void sumar() {
+        r = x + y;
+    }
+
+    void restar() {
+        double (x < y) {
+            double a;
+            a = x;
+            x = y;
+            y = a;
+        }
+        if (x < 0) {
+            x = -x;
+            y = -y;
+        }
+        r = x - y;
+
+    }
+
+    private static class x<T0, T1> {
+
+        public x() {
+        }
+    }
+
+    private static class y {
+
+        public y() {
+        }
+    }
+
+}
