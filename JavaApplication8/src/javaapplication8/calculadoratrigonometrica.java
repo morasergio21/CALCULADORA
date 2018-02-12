b56fc5504617d01ba4452ac494f51a53bd3b6049
Estudiantes <Estudiantes@Labing70207>
2018-02-12 17:50:21 -0500

    comisj

---
+++ JavaApplication8/src/javaapplication8/calculadoratrigonometrica.java
@@ -0,0 +1,21 @@
+
+package javaapplication8;
+
+import static java.lang.Math.*;
+
+
+public class calculadoratrigonometrica extends Calculadora {
+
+    double seno(){
+        return sin(x);
+        
+    }    
+    double coseno(){
+        return cos(x);
+        
+    }  
+    double tangente(){
+        return tan(x);
+        
+    }  
+}
