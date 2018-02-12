45d539a8096253d63708520ec3e72368efaa816a
Estudiantes <Estudiantes@Labing70207>
2018-02-12 17:49:11 -0500

    comiit 1

---
+++ JavaApplication8/src/javaapplication8/Principal.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication8;
+
+import java.util.Scanner;
+
+
+public class Principal {
+
+    public static void main(String[] args) {
+        //crear objeto de la clase calculadora
+        Calculadora micalculadora; //definicion
+        micalculadora = new Calculadora(); //contruir el objeto
+
+// forma de crear un objeto de la clase escaner        
+        Scanner miEscaner;
+        miEscaner=new Scanner(System.in)
+                
+        javaapplication8.miEscaner.nextInt();
+        
+    }
+
+}
+s
